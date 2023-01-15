2f6a33679fe71ce6be346c5f02ee70a0d70aec44
Positiviste <106838114+Positiviste@users.noreply.github.com>
2023-01-15 09:45:49 +0100

    Computer make a choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("Hello World !")
+console.log("Hello World !");
+function getComputerChoice () {
+    let computerChoice = Math.floor(Math.random()*3);
+    if (computerChoice == 0 ) {
+        computerChoice="Rock";
+    } else if (computerChoice == 1) {
+        computerChoice="Paper";
+    } else {
+        computerChoice="Scissors";
+    }
+    return (computerChoice);
+}
+
+for (let i = 0 ; i < 20 ; i++) {
+    console.log(getComputerChoice());
+}
+console.log("Yes !");
