29bf9f15f40912768205fb65c04183a43b1f1c17
Positiviste <106838114+Positiviste@users.noreply.github.com>
2023-01-15 10:43:10 +0100

    Self Playing

--- javascript.js
+++ javascript.js
@@ -1,17 +1,41 @@
 console.log("Hello World !");
-function getComputerChoice () {
-    let computerChoice = Math.floor(Math.random()*3);
-    if (computerChoice == 0 ) {
-        computerChoice="Rock";
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0) {
+        computerChoice = "Rock";
     } else if (computerChoice == 1) {
-        computerChoice="Paper";
+        computerChoice = "Paper";
     } else {
-        computerChoice="Scissors";
+        computerChoice = "Scissors";
     }
     return (computerChoice);
 }
 
-for (let i = 0 ; i < 20 ; i++) {
-    console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let ps = (playerSelection.toLowerCase());
+    let cs = (computerSelection.toLowerCase());
+    let result = "";
+    if (ps == cs) {
+        message = `It\'s a tie ! You both played ${ps} !`
+    } else if ((ps == "rock" && cs == "paper")
+        || (ps == "paper" && cs == "scissors")
+        || (ps == "scissors" && cs == "rock")) {
+        computerScore += 1
+        message = `You loose this round ! ${playerSelection} is beaten by ${computerSelection} !`;
+    } else {
+        playerScore += 1
+        message = `You win this round ! ${playerSelection} beats ${computerSelection} !`;
+    }
+    return (message);
 }
-console.log("Yes !");
+
+function game() {
+    while (playerScore < 3 && computerScore < 3) {
+        console.log(playRound(getComputerChoice(), getComputerChoice()));
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+game();
